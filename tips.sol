ade6013bb216621fcb94342a35d91fb0535a5b80
supalak3404 <supalak3404@gmail.com>
2024-08-13 15:31:59 +0700

    ลบไฟล์เก่า

--- tips.sol
+++ tips.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.26;
 
 contract tips {
     address owner;
+    Waiteress[] waitress;
+    
+
     constructor(){
         owner = msg.sender;
 
@@ -20,11 +23,36 @@ contract tips {
         return address(this).balance;
     }
 
-    //3. add user
+    //3. add structure waitress
+
+    struct Waiteress {
+        address payable walletAddress;
+        string name;
+    }
+
+    //3.2 add waitress
+    function addWaitress(address payable walletAddress,string memory name) public {
+        bool waitressExist = false;
+
+        if(waitress.length >=1){
+            for(uint i=0; i<waitress.length; i++)
+                if(waitress[i].walletAddress == walletAddress){
+                 waitressExist = true;
+                }
+
+            }
+            if(waitressExist==false){
+                waitress.push(Waiteress(walletAddress,name));
+            }
+
+    }
 
     //4. remove user
 
     //5. view user
+    function viewWaitrass() public view returns (Waiteress[] memory){
+        return waitress;
+    }
 
     //6. distribute tips
 }
